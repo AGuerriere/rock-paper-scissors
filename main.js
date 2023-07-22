6b0070695c16b9389cd854ef42463017a61a09b2
Antonio Guerriere <antonioguerriere@gmail.com>
2023-07-22 23:14:37 +0200

    implemented roundCompare function

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ function getComputerChoice(){
   let choices = ["Rock", "Paper", "Scissors"]
   return choices[rand]
 }
+
+function roundCompare(userSelection, computerSelection ){
+  let userSelectionSmall = userSelection.toLowerCase()
+  let computerSelectionSmall = computerSelection.toLowerCase()
+  if(userSelectionSmall === computerSelectionSmall) {
+    return "It'a draw!"
+  } else if(userSelectionSmall==="rock" && computerSelectionSmall === "paper"){
+    return "You lose! Paper beats rock "
+  } else if(userSelectionSmall==="paper" && computerSelectionSmall === "rock"){
+    return "You win! Paper beats rock"
+  } else if(userSelectionSmall==="rock" && computerSelectionSmall === "scissors"){
+    return "You win! Rock beat scissors"
+  } else if(userSelectionSmall==="scissors" && computerSelectionSmall === "rock"){
+    return "You lose! Rock beat scissors"
+  } else if(userSelectionSmall==="paper" && computerSelectionSmall === "scissors"){
+    return "You lose! Scissors beat paper"
+  } else if(userSelectionSmall==="scissors" && computerSelectionSmall === "paper"){
+    return "You win! Scissors beat paper"
+  } 
+}
