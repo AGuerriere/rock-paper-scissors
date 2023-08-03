ee319a2f2b7404a7478ca121adc866b6c01d6739
Antonio Guerriere <antonioguerriere@gmail.com>
2023-08-03 23:50:46 +0200

    game completed

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+let userWins = 0
+let computerWins = 0
+
+const rockButton = document.getElementById('rock')
+const paperButton = document.getElementById('paper')
+const scissorsButton = document.getElementById('scissors')
+const result = document.getElementById('result')
+const userIcon = document.getElementById('userIcon')
+const computerIcon = document.getElementById('computerIcon')
+const userScore = document.getElementById('userScore')
+const computerScore = document.getElementById('computerScore')
+
+
+
 function getComputerChoice(){
   let rand = Math.floor(Math.random() * 3)
-  let choices = ["Rock", "Paper", "Scissors"]
-  console.log(choices[rand])
-  return choices[rand]
+  let choices = ["rock", "paper", "scissors"]
+  let result = choices[rand]
+  computerIcon.innerText = convertToIcon(result)
+  return result
 }
 
 function roundCompare(userSelection, computerSelection ){
@@ -12,35 +27,55 @@ function roundCompare(userSelection, computerSelection ){
     return "It'a draw!"
   } else if(userSelectionSmall==="rock" && computerSelectionSmall === "paper"){
     computerWins++
+    computerScore.innerText = `Computer: ${computerWins}`
     return "You lose! Paper beats rock"
   } else if(userSelectionSmall==="paper" && computerSelectionSmall === "rock"){
     userWins++
+    userScore.innerText = `Player: ${userWins}`
     return "You win! Paper beats rock"
   } else if(userSelectionSmall==="rock" && computerSelectionSmall === "scissors"){
     userWins++
+    userScore.innerText = `Player: ${userWins}`
     return "You win! Rock beat scissors"
   } else if(userSelectionSmall==="scissors" && computerSelectionSmall === "rock"){
     computerWins++
+    computerScore.innerText = `Computer: ${computerWins}`
     return "You lose! Rock beat scissors"
   } else if(userSelectionSmall==="paper" && computerSelectionSmall === "scissors"){
     computerWins++
+    computerScore.innerText = `Computer: ${computerWins}`
     return "You lose! Scissors beat paper"
   } else if(userSelectionSmall==="scissors" && computerSelectionSmall === "paper"){
     userWins++
+    userScore.innerText = `Player: ${userWins}`
     return "You win! Scissors beat paper"
   } 
 }
 
-let userWins = 0
-let computerWins = 0
 
-let game = () => {
-  for(let i = 0; i < 5; i++){
-    console.log(roundCompare(prompt("Rock, Paper, Scissors?"), getComputerChoice()))
-  }
-  if(userWins > computerWins){
-    console.log(`You won ${userWins} to ${computerWins}`)
-  } if(userWins < computerWins){
-    console.log(`You lost ${userWins} to ${computerWins}`)
+
+rockButton.addEventListener('click', () => {
+  result.innerText = (roundCompare('rock', getComputerChoice()))
+  userIcon.innerText = '🪨'
+})
+
+paperButton.addEventListener('click', () => {
+  result.innerText = (roundCompare('paper', getComputerChoice()))
+  userIcon.innerText = '📄'
+})
+
+scissorsButton.addEventListener('click', () => {
+  result.innerText = (roundCompare('scissors', getComputerChoice()))
+  userIcon.innerText = '✂️'
+})
+
+
+function convertToIcon(text){
+  if(text === 'rock') {
+    return "🪨"
+  } else if(text === 'paper'){
+    return "📄"
+  } else if(text === 'scissors'){
+    return "✂️"
   }
 }
